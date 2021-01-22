3e2442189234bad4202fc63d41eb7985339d98de
Joseph Guajardo <josephguajardo@ymail.com>
2021-01-21 18:03:22 -0800

    adding querySelectors and variables

---
+++ Assets/index.js
@@ -0,0 +1,10 @@
+// creating variables for the start button, questions box, choices, and the timer
+var startButton = document.querySelector("#startButton");
+var questionBox = document.querySelector("#questionsBox");
+var choiceButton = document.querySelector("#choices");
+var timeEl = document.querySelector(".timer");
+
+var questionCount = 0;
+var timeLeft = 45;
+var score = 0;
+var correctAnswer;
