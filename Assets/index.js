594401184f2522650aad2b06e045325689810e3e
Joseph Guajardo <josephguajardo@ymail.com>
2021-01-21 18:04:23 -0800

    adding object for questions

--- Assets/index.js
+++ Assets/index.js
@@ -8,3 +8,29 @@ var questionCount = 0;
 var timeLeft = 45;
 var score = 0;
 var correctAnswer;
+
+//creating object array for the questions, answers, and the correct answer. 
+var questions = [
+
+    {
+        q: 'What is a boolean?',
+        a: ['A red ballon', 'a true or false statement', 'a variable', 'a statement'],
+
+        correct: 'a true or false statement',
+    },
+    {
+        q: 'what is a variable',
+        a: ['the nouns of JavaScript', 'a true statement', 'a false statement', 'an array'],
+        correct: '',
+    },
+    {
+        q: 'What does an prompt do?',
+        a: ['Makes things move faster', 'makes things move slower', 'loops', 'a pop up with a text box'],
+        correct: 'a pop up with a text box',
+    },
+    {
+        q: 'What goes inside the brackets in an array?',
+        a: ['a boolean', 'a statement', 'indexes', 'more brackets'],
+        correct: 'indexes',
+    },
+];
