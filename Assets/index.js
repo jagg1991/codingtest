267a627621084b455e33b5c73f0447159fe1c74c
Joseph Guajardo <josephguajardo@ymail.com>
2021-01-22 18:16:05 -0800

    added function to loop through questions object

--- Assets/index.js
+++ Assets/index.js
@@ -3,14 +3,26 @@ var startButton = document.querySelector("#startButton");
 var questionBox = document.querySelector("#questionsBox");
 var choiceButton = document.querySelector("#choices");
 var timeEl = document.querySelector(".timer");
+var corEl = document.querySelector("#cor");
+var inCorEl = document.querySelector("in-cor")
 
 var questionCount = 0;
 var timeLeft = 45;
 var score = 0;
 var correctAnswer;
+var corAnswer = 0;
+var inCorAnswer = 0;
+
+
+var h1El = document.createElement("h1");
+var btEl1 = document.createElement("button");
+var btEl2 = document.createElement("button");
+var btEl3 = document.createElement("button");
+var btEl4 = document.createElement("button");
+var h2El = document.createElement("h2");
 
 //creating object array for the questions, answers, and the correct answer. 
-var questions = [
+var objects = [
 
     {
         q: 'What is a boolean?',
@@ -21,7 +33,7 @@ var questions = [
     {
         q: 'what is a variable',
         a: ['the nouns of JavaScript', 'a true statement', 'a false statement', 'an array'],
-        correct: '',
+        correct: 'the nouns of JavaScript',
     },
     {
         q: 'What does an prompt do?',
@@ -33,34 +45,71 @@ var questions = [
         a: ['a boolean', 'a statement', 'indexes', 'more brackets'],
         correct: 'indexes',
     },
+
+
+
 ];
 
+
 startButton.addEventListener("click", function setTime() {
+
+    questionBox.appendChild(h1El);
+    choiceButton.appendChild(btEl1);
+    choiceButton.appendChild(btEl2);
+    choiceButton.appendChild(btEl3);
+    choiceButton.appendChild(btEl4);
+    choiceButton.appendChild(h2El);
     // Sets interval in variable
+    sendMessage();
     var timerInterval = setInterval(function () {
         timeLeft--;
         timeEl.textContent = timeLeft;
-        var h1El = document.createElement("h1");
-        var btEl = document.createElement("button");
-        questionBox.appendChild(h1El);
-        choiceButton.appendChild(btEl);
 
-        for (i = 0; i < questions.length; i++)
+        if (timeLeft === 0) {
+            // Stops execution of action at set interval
+            clearInterval(timerInterval);
 
-            if (secondsLeft === 0) {
-                // Stops execution of action at set interval
-                clearInterval(timerInterval);
-                // Calls function to create and append image
-                sendMessage();
-            }
+            // Calls function to create and append image
+            //sendMessage();
+        }
 
     }, 1000);
 });
 
 function sendMessage() {
-    timeEl.textContent = " ";
-    mainEl.appendChild(imgEl);
 
+    if (questionCount >= objects.length) {
+        return console.log("Game Over!")
+    }
+    h1El.textContent = objects[questionCount].q;
+    btEl1.textContent = objects[questionCount].a[0];
+    btEl2.textContent = objects[questionCount].a[1];
+    btEl3.textContent = objects[questionCount].a[2];
+    btEl4.textContent = objects[questionCount].a[3];
+    correctAnswer = objects[questionCount].correct;
+    questionCount++; //increasing index of object
 }
 
-setTime();
+//setTime();
+
+
+choiceButton.addEventListener("click", function (event) {
+
+    var element = event.target;
+    if (element.matches("button")) {
+        var userChoice = event.target.textContent;
+        if (userChoice === correctAnswer) {
+            timeLeft += 10;
+            h2El.textContent = "Correct!"
+            sendMessage();
+        }
+        else {
+            timeLeft -= 10;
+            h2El.textContent = "Incorrect!"
+            sendMessage();
+        }
+
+
+
+    }
+});
